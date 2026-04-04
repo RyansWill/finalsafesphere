4dbb321f1b25200fe206e8070c37cc98edd929b3
RyansWill <ryanwilfried222.com>
2026-04-04 10:55:16 +0200

    Fix: updated catch-all route in app.js for navigation

--- api/app.js
+++ api/app.js
@@ -26,7 +26,7 @@ app.use("/api/test", testRoute);
 app.use("/api/track", trackRoute);
 
 app.use(express.static(path.join(__dirname,  "dist")));
-app.get("/", (req,res) => {
+app.use((req,res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 })
 
